a469c381d218faf458367cfd38d2231a72cd3883
mrome007 <mrome007@ucr.edu>
2017-11-27 14:02:25 -0800

    Initial commit

---
+++ Assets/Scripts/RectUtility.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RectUtility : MonoBehaviour 
+{
+	static Texture2D whiteTexture;
+	public static Texture2D WhiteTexture
+	{
+		get
+		{
+			if(whiteTexture == null)
+			{
+				whiteTexture = new Texture2D(1, 1);
+				whiteTexture.SetPixel(0, 0, Color.white);
+				whiteTexture.Apply();
+			}
+
+			return whiteTexture;
+		}
+	}
+
+	public static void DrawScreenRect(Rect rect, Color color)
+	{
+		GUI.color = color;
+		GUI.DrawTexture(rect, WhiteTexture);
+		GUI.color = Color.white;
+	}
+
+	public static void DrawScreenRectBorder(Rect rect, float thickness, Color color)
+	{
+		DrawScreenRect(new Rect(rect.xMin, rect.yMin, rect.width, thickness), color);
+		DrawScreenRect(new Rect(rect.xMin, rect.yMin, thickness, rect.height), color);
+		DrawScreenRect(new Rect(rect.xMax - thickness, rect.yMin, thickness, rect.height), color);
+		DrawScreenRect(new Rect(rect.xMin, rect.yMax - thickness, rect.width, thickness), color);
+	}
+
+	public static Rect GetScreenRect(Vector3 screenPosition1, Vector3 screenPosition2)
+	{
+		screenPosition1.y = Screen.height - screenPosition1.y;
+		screenPosition2.y = Screen.height - screenPosition2.y;
+
+		var topLeft = Vector3.Min(screenPosition1, screenPosition2);
+		var bottomRight = Vector3.Max(screenPosition1, screenPosition2);
+
+		return Rect.MinMaxRect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y);
+	}
+
+	public static Bounds GetViewportBounds(Camera camera, Vector3 screenPosition1, Vector3 screenPosition2)
+	{
+		var v1 = Camera.main.ScreenToViewportPoint(screenPosition1);
+		var v2 = Camera.main.ScreenToViewportPoint(screenPosition2);
+
+		var min = Vector3.Min(v1, v2);
+		var max = Vector3.Max(v1, v2);
+
+		min.z = camera.nearClipPlane;
+		max.z = camera.farClipPlane;
+
+		var bounds = new Bounds();
+		bounds.SetMinMax(min, max);
+		return bounds;
+	}
+}
---
+++ Assets/Scripts/UnitSelection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSelection : MonoBehaviour 
+{
+	public List<Transform> Units;
+
+	private bool isSelecting = false;
+	private Vector3 mousePosition1;
+
+	private void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			isSelecting = true;
+			mousePosition1 = Input.mousePosition;
+		}
+
+		if(Input.GetMouseButtonUp(0))
+		{
+			if(isSelecting)
+			{
+				for(int index = 0; index < Units.Count; index++)
+				{
+					if(IsWithinSelectionBounds(Units[index]))
+					{
+						Debug.Log(Units[index].name);
+					}
+				}
+			}
+			isSelecting = false;
+		}
+	}
+
+	private void OnGUI()
+	{
+		if(isSelecting)
+		{
+			var rect = RectUtility.GetScreenRect(mousePosition1, Input.mousePosition);
+			RectUtility.DrawScreenRect(rect, new Color(0.8f, 0.8f, 0.95f, 0.25f));
+			RectUtility.DrawScreenRectBorder(rect, 2, new Color(0.8f, 0.8f, 0.95f));
+		}
+	}
+
+	private bool IsWithinSelectionBounds(Transform unitTransform)
+	{
+		if(!isSelecting)
+		{
+			return false;
+		}
+
+		var camera = Camera.main;
+		var viewportBounds = RectUtility.GetViewportBounds(camera, mousePosition1, Input.mousePosition);
+
+		return viewportBounds.Contains(camera.WorldToViewportPoint(unitTransform.position));
+	}
+}
